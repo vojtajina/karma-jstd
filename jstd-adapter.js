ffc2c8946cc467fa28d227245effd57edc59c825
tcorral <amischol@gmail.com>
2013-04-27 20:26:37 +0200

    Fix all the reviewed code.

--- jstd-adapter.js
+++ jstd-adapter.js
@@ -11,45 +11,30 @@
  */
 
 
-var __jstd_specs = {}, startJasmine, assert, assertTrue;
+var __jstd_specs = {}, startJasmine, assert, assertTrue, ASYNC_TYPE = 'async';
 
-/**
- * TestCase replacement that will use Jasmine describe and it functions.
- * @param name
- * @param proto
- * @param type
- * @constructor
- */
 function TestCase(name, proto, type) {
   var prototype = __jstd_specs[name] = proto || {};
 
   return {
     prototype: prototype
   };
-};
+}
 
-/**
- * ConditionalTestCase replacement to create a special testcase.
- * @param name
- * @param condition
- * @param proto
- * @param type
- * @returns {*}
- * @constructor
- */
+function AsyncTestCase(name, proto) {
+  return TestCase(name, proto, ASYNC_TYPE);
+}
+
 function ConditionalTestCase(name, condition, proto, type) {
   if (condition()) {
     return TestCase(name, proto, type);
   }
 }
 
-// helper functions
-function emptyFunc() {
+function ConditionalAsyncTestCase(name, condition, proto){
+  return ConditionalTestCase(name, condition, proto, ASYNC_TYPE);
 }
-/**
- * injectHTML is a function to add content to the body of the page.
- * @param sHTML
- */
+
 function injectHTML(sHTML) {
   var docFrag = document.createDocumentFragment(),
     layer = document.createElement('div'),
@@ -66,18 +51,7 @@ function injectHTML(sHTML) {
 }
 
 // matchers
-/**
- * Throws an error.
- * @param sMessage
- */
-function fail(sMessage) {
-  throw new Error(sMessage);
-}
-/**
- * Checks if actual is true.
- * @param message
- * @param actual
- */
+
 assert = assertTrue = function (message, actual) {
   if (arguments.length < 2) {
     actual = message;
@@ -85,148 +59,98 @@ assert = assertTrue = function (message, actual) {
 
   expect(actual).toBeTruthy();
 };
-/**
- * Checks if actual is false.
- * @param message
- * @param actual
- */
-function assertFalse(message, actual) {
+
+var fail = function (sMessage) {
+  throw new Error(sMessage);
+};
+
+var assertFalse = function (message, actual) {
   if (arguments.length < 2) {
     actual = message;
   }
 
   expect(actual).toBeFalsy();
-}
-/**
- * Checks that expected is equals to actual
- * Warning the jstd assertEquals is == while jasmine toEqual is ===
- * @param message
- * @param expected
- * @param actual
- */
-function assertEquals(message, expected, actual) {
+};
+
+var assertEquals = function (message, expected, actual) {
   if (arguments.length < 3) {
     actual = expected;
     expected = message;
   }
 
   expect(actual).toEqual(expected);
-}
-/**
- * Checks that actual is not equals to expected
- * @param message
- * @param expected
- * @param actual
- */
-function assertNotEquals(message, expected, actual) {
+};
+
+var assertNotEquals = function (message, expected, actual) {
   if (arguments.length < 3) {
     actual = expected;
     expected = message;
   }
 
   expect(actual).not.toEqual(expected);
-}
-/**
- * Checks that actual and expected are the same object
- * @param message
- * @param expected
- * @param actual
- */
-function assertSame(message, expected, actual) {
+};
+
+var assertSame = function (message, expected, actual) {
   if (arguments.length < 3) {
     actual = expected;
     expected = message;
   }
 
   expect(actual).toBe(expected);
-}
-/**
- * Checks that actual and expected are not the same object
- * @param message
- * @param expected
- * @param actual
- */
-function assertNotSame(message, expected, actual) {
+};
+
+var assertNotSame = function (message, expected, actual) {
   if (arguments.length < 3) {
     actual = expected;
     expected = message;
   }
 
   expect(oActual).not.toBe(oExpected);
-}
-/**
- * Checks that actual is null
- * @param message
- * @param actual
- */
-function assertNull(message, actual) {
+};
+
+var assertNull = function (message, actual) {
   if (arguments.length < 2) {
     actual = message;
   }
   expect(actual).toBeNull();
-}
-/**
- * Checks that actual is not null
- * @param message
- * @param actual
- */
-function assertNotNull(message, actual) {
+};
+
+var assertNotNull = function (message, actual) {
   if (arguments.length < 2) {
     actual = message;
   }
   expect(actual).not.toBeNull();
-}
-/**
- * Checks that actual is undefined
- * @param message
- * @param actual
- */
-function assertUndefined(message, actual) {
+};
+
+var assertUndefined = function (message, actual) {
   if (arguments.length < 2) {
     actual = message;
   }
   expect(actual).toBeUndefined();
-}
-/**
- * Checks that actual is not undefined
- * @param message
- * @param actual
- */
-function assertNotUndefined(message, actual) {
+};
+
+var assertNotUndefined = function (message, actual) {
   if (arguments.length < 2) {
     actual = message;
   }
   expect(actual).not.toBeUndefined();
-}
-/**
- * Checks that actual is NaN (not a number)
- * @param message
- * @param actual
- */
-function assertNaN(message, actual) {
+};
+
+var assertNaN = function (message, actual) {
   if (arguments.length < 2) {
     actual = message;
   }
   expect(isNaN(actual)).toBeTruthy();
-}
-/**
- * Checks that actual is not NaN (not a number)
- * @param message
- * @param actual
- */
-function assertNotNaN(message, actual) {
+};
+
+var assertNotNaN = function (message, actual) {
   if (arguments.length < 2) {
     actual = message;
   }
   expect(isNaN(actual)).toBeFalsy();
-}
-/**
- * CheckS that executing callback throws an exception
- * @param message
- * @param callback
- * @param error
- */
-function assertException(message, callback, error) {
+};
+
+var assertException = function (message, callback, error) {
   if (arguments.length < 3) {
     error = callback;
     callback = message;
@@ -238,14 +162,9 @@ function assertException(message, callback, error) {
   } else {
     expect(callback).toThrow(error);
   }
-}
-/**
- * Checks that executing callback does not throws an Exception
- * @param message
- * @param callback
- * @param error
- */
-function assertNoException(message, callback, error) {
+};
+
+var assertNoException = function (message, callback, error) {
   if (arguments.length < 3) {
     error = callback;
     callback = message;
@@ -257,199 +176,137 @@ function assertNoException(message, callback, error) {
   } else {
     expect(callback).not.toThrow(error);
   }
-}
-/**
- * Checks that actual is an Array
- * @param message
- * @param actual
- */
-function assertArray(message, actual) {
+};
+
+var assertArray = function (message, actual) {
   if (arguments.length < 2) {
     actual = message;
   }
 
   expect(typeof actual === 'array' || actual instanceof Array).toBeTruthy();
-}
-/**
- * Checks that expected is the expected type
- * @param message
- * @param expected
- * @param type
- */
-function assertTypeOf(message, expected, type) {
+};
+
+var assertTypeOf = function (message, expected, type) {
   if (arguments.length < 3) {
     type = expected;
     expected = message;
   }
 
   expect(typeof type === expected).toBeTruthy();
-}
-/**
- * Checks that actual is boolean type
- * @param message
- * @param actual
- */
-function assertBoolean(message, actual) {
+};
+
+var assertBoolean = function (message, actual) {
   if (arguments.length < 2) {
     actual = message;
   }
 
   assertTypeOf(message, 'boolean', actual);
-}
-/**
- * Checks that actual is function type
- * @param message
- * @param actual
- */
-function assertFunction(message, actual) {
+};
+
+var assertFunction = function (message, actual) {
   if (arguments.length < 2) {
     actual = message;
   }
 
   assertTypeOf(message, 'function', actual);
-}
-/**
- * Checks that actual is object type
- * @param message
- * @param actual
- */
-function assertObject(message, actual) {
+};
+
+var assertObject = function (message, actual) {
   if (arguments.length < 2) {
     actual = message;
   }
 
   assertTypeOf(message, 'object', actual);
-}
-/**
- * Checks that actual is number type
- * @param message
- * @param actual
- */
-function assertNumber(message, actual) {
+};
+
+var assertNumber = function (message, actual) {
   if (arguments.length < 2) {
     actual = message;
   }
 
   assertTypeOf(message, 'number', actual);
-}
-/**
- * Checks that actual is string type
- * @param message
- * @param actual
- */
-function assertString(message, actual) {
+};
+
+var assertString = function (message, actual) {
   if (arguments.length < 2) {
     actual = message;
   }
 
   assertTypeOf(message, 'string', actual);
-}
-/**
- * Checks that actual matches regexp
- * @param message
- * @param regexp
- * @param actual
- */
-function assertMatch(message, regexp, actual) {
+};
+
+var assertMatch = function (message, regexp, actual) {
   if (arguments.length < 2) {
     actual = regexp;
     regexp = message;
   }
 
   expect(actual).toMatch(regexp);
-}
-/**
- * Checks that actual does not matches oRegExp
- * @param message
- * @param regexp
- * @param actual
- */
-function assertNoMatch(message, regexp, actual) {
+};
+
+var assertNoMatch = function (message, regexp, actual) {
   if (arguments.length < 2) {
     actual = regexp;
     regexp = message;
   }
 
   expect(actual).not.toMatch(regexp);
-}
-/**
- * Checks that element's node name is equals to tag
- * @param message
- * @param tag
- * @param element
- */
-function assertTagName(message, tag, element) {
+};
+
+var assertTagName = function (message, tag, element) {
   if (arguments.length < 3) {
     element = tag;
     tag = message;
   }
 
   expect(element.nodeName.toLowerCase() === tag.toLowerCase()).toBeTruthy();
-}
-/**
- * Checks that element's className contains className
- * @param message
- * @param className
- * @param element
- */
-function assertClassName(message, className, element) {
+};
+
+var assertClassName = function (message, className, element) {
   if (arguments.length < 3) {
     element = className;
     className = message;
   }
 
   expect(element.className.split(" ")).toContain(className);
-}
-/**
- * Checks that element's id is equals to id
- * @param message
- * @param id
- * @param element
- */
-function assertElementId(message, id, element) {
+};
+
+var assertElementId = function (message, id, element) {
   if (arguments.length < 3) {
     element = id;
     id = message;
   }
 
   expect(oElement.id).toEqual(id);
-}
-/**
- * Checks that actual has been created from constructor
- * @param message
- * @param constructor
- * @param actual
- */
-function assertInstanceOf(message, constructor, actual) {
+};
+
+var assertInstanceOf = function (message, constructor, actual) {
   if (arguments.length < 3) {
     actual = constructor;
     constructor = message;
   }
 
   expect(actual instanceof constructor).toBeTruthy();
-}
-/**
- * Checks that actual has been created from constructor
- * @param message
- * @param constructor
- * @param actual
- */
-function assertNotInstanceOf(message, constructor, actual) {
+};
+
+var assertNotInstanceOf = function (message, constructor, actual) {
   if (arguments.length < 3) {
     actual = constructor;
     constructor = message;
   }
 
   expect(actual instanceof constructor).toBeFalsy();
-}
+};
 
 startJasmine = window.__karma__.start;
 window.__karma__.start = function () {
   // register all jstd tests as jasmine specs, before starting jasmine
   Object.keys(__jstd_specs).forEach(function (testName) {
     var prototype = __jstd_specs[testName],
-      setUp = prototype.setUp || emptyFunc,
-      tearDown = prototype.tearDown || emptyFunc,
+      setUp = prototype.setUp || function () {
+      },
+      tearDown = prototype.tearDown || function () {
+      },
       specNames = Object.keys(prototype);
 
     // setUp and tearDown callbacks are executed as beforeEach and afterEach
